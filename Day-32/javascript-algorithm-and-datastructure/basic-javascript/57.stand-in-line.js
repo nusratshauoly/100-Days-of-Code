5b1fd11b133fa24e5fa1b8f582560340b9c23490
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-01-06 11:58:34 +0600

    javascript : 57 - 63

--- Day-32/javascript-algorithm-and-datastructure/basic-javascript/57.stand-in-line.js
+++ Day-32/javascript-algorithm-and-datastructure/basic-javascript/57.stand-in-line.js
@@ -15,3 +15,15 @@ Waiting:nextInLine([2], 1) should return 2
 Waiting:nextInLine([5,6,7,8,9], 1) should return 5
 Waiting:After nextInLine(testArr, 10), testArr[4] should be 10
 */
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    return arr.shift();
+    // Only change code above this line
+  }
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
